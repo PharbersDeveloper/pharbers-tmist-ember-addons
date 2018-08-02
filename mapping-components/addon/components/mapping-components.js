4696bca5da858c6f0cd7a8b63daa952cf3e40416
Faiz <891366402@qq.com>
2018-08-02 11:56:51 +0800

    漏交

---
+++ mapping-components/addon/components/mapping-components.js
@@ -0,0 +1,17 @@
+import Component from '@ember/component';
+import { computed } from '@ember/object';
+import layout from '../templates/components/mapping-components';
+
+export default Component.extend({
+		layout,
+		result: computed('data', function() {
+			let reVal = this.get('data')
+				.map(function(ele) {
+					return { 'key': ele.key, 'values': ele.values }
+				});
+			return reVal;
+		}),
+	})
+	.reopenClass({
+		positionalParams: ['data']
+	});
---
+++ mapping-components/app/components/mapping-components.js
@@ -0,0 +1,1 @@
+export { default } from 'mapping-components/components/mapping-components';
