41c51029a91f782b99bbe7b54701876d10eb0870
Faiz <891366402@qq.com>
2018-08-13 18:51:01 +0800

    管理决策初版完成，修补了各种小问题

--- pharbers-tm-cards/addon/components/manager-times.js
+++ pharbers-tm-cards/addon/components/manager-times.js
@@ -1,9 +1,61 @@
 import Component from '@ember/component';
+import EmberObject, { observer }from '@ember/object';
+import { once } from '@ember/runloop';
 import layout from '../templates/components/manager-times';
 import styles from '../styles/manager-times';
 
+const managerTime = EmberObject.extend({
+	managerArraryObject: [
+		{lable: "KPI分析", key: "kpi", value: ""},
+		{lable: "团建/例会", key: "team_building", value: ""},
+		{lable: "行政事务", key: "administrative", value: ""}
+	],
+	init() {
+		this._super(...arguments);
+		try {
+			let obj = JSON.parse(localStorage.getItem('manager_time'));
+			if(obj.uuid === this.uuid) {
+				this.set('managerArraryObject', obj.values)
+			}
+		} catch (e) {
+			window.console.error('manager_time is not found')
+		}
+	},
+
+});
+
 export default Component.extend({
+	init() {
+		this._super(...arguments);
+		this.set('uuid', '1001010000001101010');
+		this.set('manager_time_inputs', managerTime.create({uuid: this.uuid}));
+		const reducer = (accumulator, currentValue) => accumulator + currentValue;
+		let repObj = JSON.parse(localStorage.getItem('manager_rep_time'));
+		let coach = repObj.values.map(function(elem){
+			return parseInt(elem.attrs.find(function(ele){
+				return ele.key === 'coach'
+			}).value || 0)
+		}).reduce(reducer);
+		let assist = repObj.values.map(function(elem){
+			return parseInt(elem.attrs.find(function(ele){
+				return ele.key === 'assist'
+			}).value || 0)
+		}).reduce(reducer);
+		this.set('coach', coach);
+		this.set('assist', assist);
+	},
 	layout,
 	styles,
-	tagName: '',
+	watchData: observer('manager_time_inputs.managerArraryObject.@each.value', function() {
+		once(this, 'execute');
+	}),
+	execute() {
+		let object = {
+			uuid: this.uuid,
+			values: this.manager_time_inputs.managerArraryObject
+		}
+		localStorage.setItem('manager_time', JSON.stringify(object));
+		this.sendAction('managerTime', this.manager_time_inputs.managerArraryObject);
+	},
+
 });
--- pharbers-tm-decision/addon/components/leader-decision.js
+++ pharbers-tm-decision/addon/components/leader-decision.js
@@ -19,6 +19,28 @@ export default Component.extend({
 		},
 		close() {
 			this.sendAction('close')
+		},
+		managerTime(data) {
+			let reval = data.find(function(elem){
+				return elem.key === "team_building";
+			});
+			this.set('team_building', reval.value)
+		},
+		managerTimeRep() {
+			const reducer = (accumulator, currentValue) => accumulator + currentValue;
+			let repObj = JSON.parse(localStorage.getItem('manager_rep_time'));
+			let coach = repObj.values.map(function(elem){
+				return parseInt(elem.attrs.find(function(ele){
+					return ele.key === 'coach'
+				}).value || 0)
+			}).reduce(reducer);
+			let assist = repObj.values.map(function(elem){
+				return parseInt(elem.attrs.find(function(ele){
+					return ele.key === 'assist'
+				}).value || 0)
+			}).reduce(reducer);
+			this.set('coach', coach);
+			this.set('assist', assist);
 		}
 	}
 
