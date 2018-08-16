0d6073d04526f019335a30287a1916ba4210cb61
Faiz <891366402@qq.com>
2018-08-16 15:40:25 +0800

    可能存在bug，测出来再说

--- pharbers-tm-cards/addon/components/target-detail-medicines-card.js
+++ pharbers-tm-cards/addon/components/target-detail-medicines-card.js
@@ -1,109 +1,66 @@
 import Component from '@ember/component';
 import layout from '../templates/components/target-detail-medicines-card';
 import styles from '../styles/target-detail-medicines-card';
-import EmberObject, { observer } from '@ember/object';
+import EmberObject, { observer, computed } from '@ember/object';
 import { once } from '@ember/runloop';
+import HospitalDetailsMixin from '../mixin/hospital-details';
 
-const medicObject = EmberObject.extend({
-	hospitalMedicArrayObject: [
-		{ lable: "指标设定", preset: 500, key: "target", unit: "元", sublable: "指标增长", value: "", subvalue: "" },
-		{ lable: "预算分配", preset: 500, key: "budget", unit: "元", sublable: "预算所占", value: "", subvalue: "" },
-		{ lable: "时间分配", preset: 500, key: "time", unit: "天", sublable: " ", value: "" },
-
-	],
+const medicObject = EmberObject.extend(HospitalDetailsMixin, {
 	init() {
 		this._super(...arguments);
-		let mid = this.mid;
-		let data = this.data;
-		let uuid = this.uuid;
-		let hospid = this.hospid;
-		// 获取缓存中是否存在
-		try {
-			let medicObj = JSON.parse(localStorage.getItem('hosp_medic'));
-			throw "myException";
-		} catch (e) {
-			console.log("xxxxxxxxxxxxxxxxx")
-			let init = data.map((ele) => {
-
-				return {
-					medicid: ele.id,
-					attrs: [
-						{ lable: "指标设定", preset: 500, key: "target", unit: "元", sublable: "指标增长", value: "", subvalue: "" },
-						{ lable: "预算分配", preset: 500, key: "budget", unit: "元", sublable: "预算所占", value: "", subvalue: "" },
-						{ lable: "时间分配", key: "time", unit: "天", sublable: " ", value: "" },
-					],
-				};
-			})
-			let object = {
-				uuid: uuid,
-				values: [{
-					hospid: hospid,
-					values: init
-				}]
+		this.set('hospitalMedicArrayObject', this.data.map(function(elem) {
+			return {
+				medicid: elem.id,
+				attrs: [
+					{lable: "指标设定", medicid: elem.id, key: "indicators_setting", value: "", unit: "元", computed_field: "指标增长", computedVal: 0},
+					{lable: "预算分配", medicid: elem.id, key: "budget_setting", value: "", unit: "元", computed_field: "预算分配%", computedVal: 0},
+					{lable: "时间分配", medicid: elem.id, key: "time_setting", value: "", unit: "天", computed_field: "", computedVal: ""}
+				]
 			}
-			localStorage.setItem('hosp_medic', JSON.stringify(object));
-
-		}
+		}));
+		this.initHospitalMedicinesArrary(this.uuid, this.hospital_id)
 	},
 });
 
 export default Component.extend({
+	flatten(arr) {
+        while (arr.some(item => Array.isArray(item))) {
+            arr = [].concat(...arr);
+        }
+        return arr;
+    },
 	init() {
 		this._super(...arguments);
-		this.set('hospid', "5b641342aa8de31ed8fb11e3");
-		this.set('uuid', '1001010000001101010');
-		this.set('hosp_medic_input', medicObject.create({ uuid: this.uuid, hospid: this.hospid, data: this.data }))
-		// this.total = [];
-		// this.mapData(this.data);
-		// this.observerArea();
+		this.set('hosp_medic_input', medicObject.create({ data: this.data, uuid: this.uuid, hospital_id: this.hospital_id }));
 	},
-
+	medicines: computed('hosp_medic_input.hospitalMedicArrayObject.@each', function() {
+		return this.flatten(this.get('hosp_medic_input.hospitalMedicArrayObject'));
+	}),
+	values: computed('medicines.@each.attrs', function() {
+		return this.flatten(this.get('medicines').mapBy('attrs'));
+	}),
+	watchData: observer('values.@each.value', function() {
+		once(this, 'execute');
+	}),
+	execute() {
+		let medicines = JSON.parse(localStorage.getItem('hospital_medicines'));
+		let hospital_id = this.hospital_id
+		if(this.uuid == medicines.uuid) {
+			let object = medicines.values.filter(function(elem){
+				return elem.hospital_id != hospital_id
+			});
+			object.pushObject({
+				hospital_id: this.hospital_id,
+				medicines: this.get('medicines')
+			});
+			medicines.values.clear();
+			medicines.values = object
+		}
+		localStorage.setItem('hospital_medicines', JSON.stringify(medicines))
+	},
 	layout,
 	styles,
 	tagName: 'div',
 	localClassNames: 'medicine-info',
 	classNames: ['container'],
-	watchData: observer('hosp_medic_input.hospitalMedicArrayObject.@each.value', function() {
-		once(this, 'watchTarget')
-	}),
-	watchTarget: function() {
-		let hmid = JSON.parse(localStorage.getItem('hosp_medic'));
-		console.log(hmid)
-		// let hopid = this.hospid;
-		let hospid = "5b641342aa8de31ed8fb11e3";
-		let medicid = "5b435533ed925c05565b5c2c";
-		if (hmid.uuid === this.uuid) {
-			for (let i = 0, len = hmid.values.length; i < len; i++) {
-				let hospValues = hmid.values.filter((ele) => {
-					ele.hospid !== hospid
-				});
-				if (hmid.values[i].hospid === this.hospid) {
-					let medicValues = hmid.values[i].values.filter((ele) => {
-						return ele.medicid !== medicid
-					})
-					let _inputData = this.hosp_medic_input.hospitalMedicArrayObject;
-					_inputData.map((ele) => {
-						if (ele.subvalue !== undefined && ele.value !== "") {
-							Ember.set(ele, 'subvalue', (Number(ele.value) / ele.preset).toFixed(2));
-						}
-					});
-					medicValues.pushObject({
-						medicid: medicid,
-						attrs: _inputData
-						// attrs: this.hosp_medic_input.hospitalMedicArrayObject
-					})
-
-					hospValues.pushObject({
-						hospid: this.hospid,
-						values: medicValues
-					})
-					let object = {
-						uuid: this.uuid,
-						values: hospValues
-					}
-					localStorage.setItem('hosp_medic', JSON.stringify(object))
-				}
-			}
-		}
-	},
 });
---
+++ pharbers-tm-cards/addon/mixin/hospital-details.js
@@ -0,0 +1,33 @@
+import Mixin from '@ember/object/mixin';
+
+export default Mixin.create({
+    // hospitalMedicArrayObject: [
+    //     {lable: "指标设定", key: "indicators_setting", value: ""},
+    //     {lable: "预算分配", key: "budget_setting", value: ""},
+    //     {lable: "时间分配", key: "time_setting", value: ""}
+	// ],
+    initHospitalMedicinesArrary(uuid, hospitalId) {
+        let init = function() {
+            let object = {
+                uuid: uuid,
+                values: []
+            }
+            localStorage.setItem('hospital_medicines', object);
+        }
+        let medicines = localStorage.getItem('hospital_medicines');
+        if(medicines == null || medicines == undefined) {
+            init()
+        } else {
+            if(JSON.parse(medicines).uuid != uuid) {
+                init()
+            } else {
+                let info = JSON.parse(medicines).values.find(function(elem) {
+                    return elem.hospital_id == hospitalId
+                });
+                if(info) {
+                    this.set('hospitalMedicArrayObject', info.medicines)
+                }
+            }
+        }
+    }
+});
