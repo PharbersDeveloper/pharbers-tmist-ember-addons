0bb35f07c277d78aa51822e6c928ba88f6f3dd17
Frank <wsgoodman@163.com>
2018-08-22 16:29:42 +0800

    创建decision 切换逻辑的连个界面。

--- pharbers-tm-all-decision/addon/components/hospital-decision.js
+++ pharbers-tm-all-decision/addon/components/hospital-decision.js
@@ -1,6 +1,14 @@
 import Component from '@ember/component';
 import layout from '../templates/components/hospital-decision';
+import styles from '../styles/hospital-decision';
 
 export default Component.extend({
-  layout
-});
+        layout,
+        styles,
+        tagName: 'div',
+        localClassNames: 'hospital-decision',
+
+    })
+    .reopenClass({
+        positionalParams: ['data']
+    });
