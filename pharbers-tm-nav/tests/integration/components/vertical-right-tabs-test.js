eff89aa27f8f3a6c53734b0b32ed3a8a4b4af998
Frank <wsgoodman@163.com>
2018-08-22 22:14:20 +0800

    新增右侧切换tab，包括预算进度条等。

--- pharbers-tm-nav/tests/integration/components/vertical-right-tabs-test.js
+++ pharbers-tm-nav/tests/integration/components/vertical-right-tabs-test.js
@@ -3,22 +3,22 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-module('Integration | Component | nav-tabs', function(hooks) {
+module('Integration | Component | vertical-right-tabs', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`{{nav-tabs}}`);
+    await render(hbs`{{vertical-right-tabs}}`);
 
     assert.equal(this.element.textContent.trim(), '');
 
     // Template block usage:
     await render(hbs`
-      {{#nav-tabs}}
+      {{#vertical-right-tabs}}
         template block text
-      {{/nav-tabs}}
+      {{/vertical-right-tabs}}
     `);
 
     assert.equal(this.element.textContent.trim(), 'template block text');
