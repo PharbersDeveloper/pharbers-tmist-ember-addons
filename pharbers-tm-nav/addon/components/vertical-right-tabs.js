cd187ad89dc215c1b460f27e580e85099d938c9d
Faiz <891366402@qq.com>
2018-09-15 01:07:16 +0800

    缓存model -> LocalStore再从 LocalStore -> model无缝结合，明明有写好的东西就是不用
    代码就不说了，改了2个组件改不动了，希望在第一次重构的时候，学会分组件与显示层，都能显示同样的效果，为啥要有父子与组装层这一说。
    
    这次我写的其实并没有太多学习价值，都是Ember Data的基本操作，与部分思想，组装层实在是改不动了。
    
    就这样吧！周一赶紧开始拼接计算逻辑，与验证

--- pharbers-tm-nav/addon/components/vertical-right-tabs.js
+++ pharbers-tm-nav/addon/components/vertical-right-tabs.js
@@ -12,25 +12,8 @@ export default Component.extend({
         this._super(...arguments);
         this.sendAction('getRandB', this);
     },
-    // 每位代表的人力进度
-    // manpowerPerc: computed('data', 'data.@each.used', function() {
-    //     let manpowerPromise = this.get('data');
-    //     let newManpower = [];
-    //     if (manpowerPromise != undefined) {
-    //         manpowerPromise.map((item) => {
-    //             let percentObject = {};
-    //             percentObject.name = item.repInfo.rep_name;
-    //             percentObject.id = item.id;
-    //             percentObject.used = item.used_days;
-    //             percentObject.total = item.total_days;
-    //             percentObject.percent = ((item.used_days / item.total_days) * 100).toFixed(0);
-    //             newManpower.push(percentObject);
-    //         })
-    //         return newManpower
-    //     }
-    // }),
+    // TODO 为毛是写死的
     calcBudget: computed('budget', function() {
-        // console.log(this.get('budget'));
         let totalBudget = {};
         totalBudget.used = this.get('budget');
         totalBudget.total = 800000;
